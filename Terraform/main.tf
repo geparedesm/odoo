da2f6e6ccbfbb1974e352212c97f37f3170c0117
Gabriel Paredes <gparedes@duodata.io>
2025-04-24 09:49:31 +0800

    [UPD] Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "ec2_instance" {
   ami             = "ami-0f5d1713c9af4fe30"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instances.name]
-  user_data = "${file("/Users/gabrielparedes/Documents/gp/aj_fencing/Terraform/deploy.sh")}"
+  user_data = "${file("/Users/gabrielparedes/Documents/gp/odoo/Terraform/deploy.sh")}"
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = 20
@@ -46,27 +46,8 @@ resource "aws_security_group" "instances" {
 resource "aws_security_group_rule" "allow_http_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
-
   from_port   = 80
   to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-resource "aws_security_group_rule" "allow_gps_tcp_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instances.id
-
-  from_port   = 9000
-  to_port     = 9000
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-resource "aws_security_group_rule" "allow_gps_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instances.id
-
-  from_port   = 5000
-  to_port     = 5000
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 }
@@ -74,7 +55,6 @@ resource "aws_security_group_rule" "allow_gps_http_inbound" {
 resource "aws_security_group_rule" "allow_odoo_http_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
-
   from_port   = 8069
   to_port     = 8069
   protocol    = "tcp"
@@ -84,7 +64,6 @@ resource "aws_security_group_rule" "allow_odoo_http_inbound" {
 resource "aws_security_group_rule" "allow_https_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
-
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
@@ -94,7 +73,6 @@ resource "aws_security_group_rule" "allow_https_inbound" {
 resource "aws_security_group_rule" "allow_ssh_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.instances.id
-
   from_port   = 22
   to_port     = 22
   protocol    = "tcp"
@@ -114,7 +92,6 @@ resource "aws_security_group_rule" "allow_nginx_inbound" {
 resource "aws_security_group_rule" "allow_outbound" {
   type              = "egress"
   security_group_id = aws_security_group.instances.id
-
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
